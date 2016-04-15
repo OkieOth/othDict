eed2b00dc10351864243f23ba2225c5ecf9dbd9b
okieOth <okie.othsam@outlook.de>
2016-04-15 23:29:13 +0200

    project initialization

---
+++ othDictRest/src/main/java/de/othsoft/othdict/rest/RestServer.java
@@ -0,0 +1,9 @@
+package de.othsoft.othdict.rest;
+
+public class RestServer {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
